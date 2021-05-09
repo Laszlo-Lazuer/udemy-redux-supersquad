d3dedde1af79467c0d06eaa18b6e60112ba207ef
A. Laszlo Lazuer <visualjazz@gmail.com>
2021-05-09 01:43:10 -0500

    chore(filter): subscribe and use reducer

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDom from 'react-dom';
 import App from './components/App'
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-
 import rootReducer from './reducers';
+import {addCharacterById} from './actions';
 
 const store = createStore(rootReducer);
 console.log('store.getState()', store.getState());
+store.subscribe(() => console.log('store', store.getState()));
+store.dispatch(addCharacterById(2));
 
 ReactDom.render(
     <Provider store={store}>
