95ed94c8784b322dd2a5c4f6305d8d0a74abf8ab
A. Laszlo Lazuer <visualjazz@gmail.com>
2021-05-09 01:33:04 -0500

    chore(redux): add state provider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
 import App from './components/App'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDom.render(<App />, document.getElementById('root'));
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer);
+console.log('store.getState()', store.getState());
+
+ReactDom.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, document.getElementById('root'));
